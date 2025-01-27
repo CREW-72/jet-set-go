615b30d96bae6b8d505cae8ffae73fde7a04dc54
Dinil Jayasuriya <w2054008@westminster.ac.uk>
2025-01-27 14:16:49 +0530

    Uploaded base flutter project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jet-set-go
 Repository for Jet.Set.Go project
+//cloned by Dinil 
---
+++ android/app/src/main/kotlin/com/example/jet_set_go/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jet_set_go
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
