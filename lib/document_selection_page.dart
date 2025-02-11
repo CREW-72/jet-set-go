2797fa95f7ec796bb422f4c079bb5f409d59ec14
Dinil Jayasuriya <w2054008@westminster.ac.uk>
2025-02-12 01:36:25 +0530

    Add background image to selection page,update pubspec.yaml to recognize assets>images

--- lib/document_selection_page.dart
+++ lib/document_selection_page.dart
@@ -24,26 +24,36 @@ class DocumentSelectionPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.jpg"), // Your background image
+          fit: BoxFit.cover, // Ensures full-screen coverage
+        ),
+      ),
+
+    child:  Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(title: Text("Select Document Type")),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _buildDocumentButton(context, "Ticket"),
-            _buildDocumentButton(context, "NIC"),
-            _buildDocumentButton(context, "Passport"),
-            _buildDocumentButton(context, "Boarding Pass"),
-            _buildDocumentButton(context, "Visa"),
-            _buildDocumentButton(context, "Prescriptions"),
-            _buildDocumentButton(context, "Other"),
-          ],
+            children: [
+              _buildDocumentButton(context, "Ticket"),
+              _buildDocumentButton(context, "NIC"),
+              _buildDocumentButton(context, "Passport"),
+              _buildDocumentButton(context, "Boarding Pass"),
+              _buildDocumentButton(context, "Visa"),
+              _buildDocumentButton(context, "Prescriptions"),
+              _buildDocumentButton(context, "Other"),
+            ],
+          ),
         ),
       ),
     );
   }
-
 
 
 }
