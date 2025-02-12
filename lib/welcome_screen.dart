a2129c82edea33e950a2c03553dd524e69d9f10c
piyumigunasekara03 <piyumigunasekara22@gmail.com>
2025-02-12 22:48:16 +0530

    Added image files in assets for the welcome_screen.dart

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/pattern.png"), // Your background pattern
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            Image.asset("assets/welcome.png", width: 200), // Your main image
+            SizedBox(height: 20),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(text: "JET.", style: TextStyle(color: Colors.blue)),
+                  TextSpan(text: "SET.", style: TextStyle(color: Colors.red)),
+                  TextSpan(text: "GO", style: TextStyle(color: Colors.yellow)),
+                ],
+              ),
+            ),
+            SizedBox(height: 40),
+            CustomButton(text: "Login", onTap: () {}),
+            SizedBox(height: 15),
+            CustomButton(text: "Sign up", onTap: () {}),
+            Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+
+  CustomButton({required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 15),
+        ),
+        onPressed: onTap,
+        child: Text(text, style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
