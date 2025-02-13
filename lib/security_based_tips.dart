fb20b2738f2907469a038c46f0fa7f6ed566677a
Vihanga33 <vihanga.20232286@iit.ac.lk>
2025-02-13 16:30:48 +0530

    changed UI of Next button in Security tips

--- lib/security_based_tips.dart
+++ lib/security_based_tips.dart
@@ -75,7 +75,12 @@ class SecurityBasedTips extends StatelessWidget {
                 }).toList(),
               ),
             ),
-            ElevatedButton.icon(
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40.0),
+              child: SizedBox(
+              width: 300,
+              height: 45,
+              child:ElevatedButton.icon(
               onPressed: () {
                 Navigator.push(
                   context,
@@ -84,8 +89,17 @@ class SecurityBasedTips extends StatelessWidget {
                   ),
                 );
               },
-              icon: Icon(Icons.arrow_forward),
+              icon: Icon(Icons.arrow_forward, color: Colors.blue,),
               label: Text("Next"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                ),
+              ),
+            ),
+            ),
             ),
           ],
         ),
