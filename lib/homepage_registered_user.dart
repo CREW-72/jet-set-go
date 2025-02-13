36ea2975270ad429797fd63a95d68eee930842de
Pahan Supurna <pahansupurna2006@gmail.com>
2025-02-13 11:16:34 +0530

    Add the background for the page and colour of navigation bar

--- lib/homepage_registered_user.dart
+++ lib/homepage_registered_user.dart
@@ -37,180 +37,215 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            bottom: 20,
-            left: 10,
-            right: 10,
-            child: Column(
-              children: [
-                // Main content area
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start, // Ensures left alignment
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'), // Replace with your image path
+            fit: BoxFit.cover, // Ensures the image covers the entire screen
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 20,
+              left: 10,
+              right: 10,
+              child: Column(
+                children: [
+                  // Main content area
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start, // Ensures left alignment
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft, // Aligns text to the left
+                          child: Text(
+                            'WELCOME BACK, USER!',
+                            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      elevation: 4,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 170),
+                              Transform.translate(
+                                offset: Offset(-70, -160), // Move 30 pixels right, 10 pixels down
+                                child: Text(
+                                  'Current Trip',
+                                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                                ),
+                              ),
+
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 3),
+                  Row(
                     children: [
-                      Align(
-                        alignment: Alignment.centerLeft, // Aligns text to the left
-                        child: Text(
-                          'WELCOME BACK, USER!',
-                          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          elevation: 4,
+                          child: InkWell(
+                            onTap: () {},
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 100),
+                                  Text(
+                                    'Documents',
+                                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 3),
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          elevation: 4,
+                          child: InkWell(
+                            onTap: () {},
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 70),
+                                  Text(
+                                    'Special Assistance',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
-                ),
 
-                SizedBox(
-                  width: double.infinity,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    elevation: 4,
-                    child: InkWell(
-                      onTap: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          children: [
-                            SizedBox(height: 170),
-                            Transform.translate(
-                              offset: Offset(-70, -160), // Move 30 pixels right, 10 pixels down
-                              child: Text(
-                                'Current Trip',
-                                style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          elevation: 4,
+                          child: InkWell(
+                            onTap: () {},
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 100),
+                                  Text(
+                                    'Transport',
+                                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                                  ),
+                                ],
                               ),
                             ),
-
-                          ],
+                          ),
                         ),
                       ),
-                    ),
+                      SizedBox(width: 3),
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          elevation: 4,
+                          child: InkWell(
+                            onTap: () {},
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 100),
+                                  Text(
+                                    'Luggage',
+                                    style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(height: 3),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        elevation: 4,
-                        child: InkWell(
-                          onTap: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Column(
-                              children: [
-                                SizedBox(height: 100),
-                                Text(
-                                  'Documents',
-                                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 3),
-                    Expanded(
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        elevation: 4,
-                        child: InkWell(
-                          onTap: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Column(
-                              children: [
-                                SizedBox(height: 70),
-                                Text(
-                                  'Special Assistance',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-                Row(
-                  children: [
-                    Expanded(
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        elevation: 4,
-                        child: InkWell(
-                          onTap: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Column(
-                              children: [
-                                SizedBox(height: 100),
-                                Text(
-                                  'Transport',
-                                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 3),
-                    Expanded(
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        elevation: 4,
-                        child: InkWell(
-                          onTap: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Column(
-                              children: [
-                                SizedBox(height: 100),
-                                Text(
-                                  'Luggage',
-                                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onNavItemTapped,
-        backgroundColor: Colors.transparent,
-        selectedItemColor: Color(0xFFACE6FC), // HEX ACE6FC for selected items
-        unselectedItemColor: Color(0xFFACE6FC), // HEX ACE6FC for unselected items
+        backgroundColor: Colors.blue[900],
+        selectedItemColor: Color(0xFFACE6FC), // Selected item color
+        unselectedItemColor: Color(0xFFACE6FC), // Unselected item color
+        type: BottomNavigationBarType.fixed, // Keeps the bar size fixed
+        showSelectedLabels: false, // Hides text labels
+        showUnselectedLabels: false, // Hides text labels
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
-          BottomNavigationBarItem(icon: Icon(Icons.lightbulb), label: 'Tips'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 10), // Moves icon lower
+              child: Icon(Icons.home),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Icon(Icons.settings),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Icon(Icons.lightbulb),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Icon(Icons.person),
+            ),
+            label: '',
+          ),
         ],
       ),
     );
