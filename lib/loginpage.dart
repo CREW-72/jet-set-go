3124411f375fda1030da70938d1a754717f74fb3
Pahan Supurna <pahansupurna2006@gmail.com>
2025-02-21 21:45:44 +0530

    Add basic Flutter login page scaffold

---
+++ lib/loginpage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Page',
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
