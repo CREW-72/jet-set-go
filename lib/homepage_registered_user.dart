a995342727a0205954c5854cd2751db422460179
Dinil Jayasuriya <w2054008@westminster.ac.uk>
2025-03-09 22:42:26 +0530

    Updated main.dart and removed redundant code from homepage_registered_user.dart

--- lib/homepage_registered_user.dart
+++ lib/homepage_registered_user.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(
-          Theme.of(context).textTheme, // This will apply Poppins globally
-        ),
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
