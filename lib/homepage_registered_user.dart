f1b1a701e12e52a3a52e2f7b3357fb71d0808171
Dinil Jayasuriya <w2054008@westminster.ac.uk>
2025-03-09 22:55:22 +0530

    Fix dart analysis issues

--- lib/homepage_registered_user.dart
+++ lib/homepage_registered_user.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
   void _onNavItemTapped(int index) {
