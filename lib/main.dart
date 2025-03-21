01777885e8bfc2d2e6e066a69705c9e1f4412199
Thinogie <td.jayasundara06@gmail.com>
2025-03-21 13:31:41 +0530

    feat: main.dart: Set LandingPage as home

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:jet_set_go/airport_navigation/navigation_landing.dart';
 import 'package:jet_set_go/landing_page/landing_page.dart'; // Set Loading Page as Home
 
 void main() async {
@@ -20,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Jet Set Go',
-      home: NavigationLanding(), // Set Landing Page as the entry point
+      home: LandingPage(), // Set Landing Page as the entry point
     );
   }
 }
