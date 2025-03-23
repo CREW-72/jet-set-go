f7c34f54f060a87a3c272cf179de8f5a11bb0685
Vihanga33 <vihanga.20232286@iit.ac.lk>
2025-03-23 10:38:36 +0530

    refactor: Rename and move search destination widget

--- lib/airport_navigation/map_screen.dart
+++ lib/airport_navigation/map_screen.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 import 'package:jet_set_go/packing_tips/style.dart'; // New style import
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:jet_set_go/airport_navigation/widgets/search_destination.dart';
+import 'package:jet_set_go/airport_navigation/search_destination.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
